d303f05b76f81adaf102e8655fcdcced1b9d9f6c
Victor Herison Pereira Patrocínio <20221151900013@ifto.local>
2024-03-14 15:08:05 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const { json } = require('body-parser');
+const express = require('express')
+app = express()
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://root:root@teste.yz7hkqq.mongodb.net/?retryWrites=true&w=majority&appName=teste');
+
+
+app.use(express.json())
+
+const Funcionarios = mongoose.model('Funcionarios',{
+    name:String,
+    idade: Number,
+    trabalho: String
+}
+)
+//podemos inserir um dado instanciando um objeto ou usando o metodo create.
+
+let funcionario1 = new Funcionarios({
+    name:'joão',
+    idade: 21,
+    trabalho: 'gari'
+})
+funcionario1.save()
+//ou
+Funcionarios.create({
+    name:'vitor',
+    idade: 19,
+    trabalho: 'pedreiro'
+})
+
+app.get('/', async (req,res) =>{
+   
+    // procura em mongoDB
+    res.send((await Funcionarios.deleteMany({name:'joão'})))   
+    
+
+
+
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`ouvindo na porta 3000`)
+  })
